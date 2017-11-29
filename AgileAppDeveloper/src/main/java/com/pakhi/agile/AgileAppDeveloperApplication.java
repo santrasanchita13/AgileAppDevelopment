908f1a43d02377e640ce6947c9cc6c19190ec813
Sanchita Santra <santra.sanchita13@gmail.com>
2017-11-29 13:19:48 +0530

    change commit try

--- AgileAppDeveloper/src/main/java/com/pakhi/agile/AgileAppDeveloperApplication.java
+++ AgileAppDeveloper/src/main/java/com/pakhi/agile/AgileAppDeveloperApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AgileAppDeveloperApplication {
 
 	public static void main(String[] args) {
+		//comment to commit
 		SpringApplication.run(AgileAppDeveloperApplication.class, args);
 	}
 }
