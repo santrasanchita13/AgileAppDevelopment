3cf848e9744a1ba002ce57179aebe6c5946bb1f6
Sanchita Santra <santra.sanchita13@gmail.com>
2017-11-29 13:13:50 +0530

    main project first commit

---
+++ AgileAppDeveloper/src/main/java/com/pakhi/agile/AgileAppDeveloperApplication.java
@@ -0,0 +1,12 @@
+package com.pakhi.agile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileAppDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileAppDeveloperApplication.class, args);
+	}
+}
