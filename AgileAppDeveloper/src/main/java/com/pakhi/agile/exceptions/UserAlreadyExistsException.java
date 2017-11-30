89b3a053bb5276015f552f1753dc9c185360de0f
Sanchita Santra <santra.sanchita13@gmail.com>
2017-11-30 15:00:58 +0530

    minor change in exception message, space was missing

--- AgileAppDeveloper/src/main/java/com/pakhi/agile/exceptions/UserAlreadyExistsException.java
+++ AgileAppDeveloper/src/main/java/com/pakhi/agile/exceptions/UserAlreadyExistsException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.CONFLICT)
 public class UserAlreadyExistsException extends RuntimeException {
 	public UserAlreadyExistsException(String emailId) {
-		super(emailId + "already exists");
+		super(emailId + " already exists");
 	}
 }
